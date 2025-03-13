46b5663d878b68bc1ea35fb4fa15e50117ddf4b6
l1shen <648952316@qq.com>
2025-03-14 01:49:48 +0800

    feat: add monaco-editor
    feat: add monaco-editor

---
+++ src/composables/editor.ts
@@ -0,0 +1,31 @@
+import { editor } from 'monaco-editor'
+import { onScopeDispose, shallowRef, watch, type Ref } from 'vue'
+
+export function useEditor(
+  model: Ref<editor.ITextModel>,
+  dom: Ref<HTMLElement | undefined>,
+  dark: Ref<boolean>,
+) {
+  const editorInstance = shallowRef<editor.IStandaloneCodeEditor>()
+
+  watch(
+    [dom, model, dark],
+    ([domValue, modelValue, darkValue]) => {
+      if (!domValue) return
+
+      editorInstance.value?.dispose()
+      editorInstance.value = editor.create(domValue, {
+        model: modelValue,
+        automaticLayout: true,
+        theme: darkValue ? 'vs-dark' : 'vs-light',
+      })
+    },
+    { immediate: true },
+  )
+
+  onScopeDispose(() => {
+    editorInstance.value?.dispose()
+  })
+
+  return { editorInstance }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './setup-monaco'
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import './global.css'
