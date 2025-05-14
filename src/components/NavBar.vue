06fe012a2e8fd8dc8f306edb69f9bd99bdf32a2f
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-15 06:34:26 +0800

    feat: add reset button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
+import * as monaco from 'monaco-editor'
 import { dark } from '../composables/dark'
+import { cmd, defaultFiles, files } from '../composables/state'
+
+function reset() {
+  if (
+    // eslint-disable-next-line no-alert
+    window.confirm(
+      'Are you sure you want to reset all files and commands to their default values?',
+    )
+  ) {
+    monaco!.editor.getModels().forEach((model) => {
+      if (model.uri.authority === 'model') return
+      model.dispose()
+    })
+    files.value = defaultFiles()
+    cmd.value = ''
+  }
+}
 </script>
 
 <template>
   <div flex self-end gap1 border rounded-full>
+    <button title="Start Over" nav-button @click="reset">
+      <div i-ri:refresh-line text-2xl />
+    </button>
+
     <button nav-button @click="dark = !dark">
       <div dark:i-ri:moon-line i-ri:sun-line text-2xl />
     </button>
